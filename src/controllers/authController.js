a85bf209dca2a5eb16c28032fb77ee8b27d691f0
Caique <caiqueandrey@hotmail.com>
2021-04-06 16:00:55 -0300

    Criando rota de autenticação com JWT

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../config/auth');
 
 const User = require('../models/User');
 
@@ -8,8 +12,9 @@ router.post('/register', async (req, res) =>{
     const { email } = req.body;
     
     try {
-        if(await User.findOne())
+        if(await User.findOne({email})){
             return res.status(400).send({ error: 'Email already exists'});
+        }
 
         const user = await User.create(req.body);
 
@@ -21,6 +26,26 @@ router.post('/register', async (req, res) =>{
     }catch(err){
         return res.status(400).send({error: 'Registration failed'});
     }
+});
+
+router.post('/authenticate', async(req, res) =>{
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if(!user)
+        return res.status(400).send({error: 'User not found'});
+
+    if(!await bcrypt.compare(password, user.password)) //comparando a senha do login com a do BD
+        return res.status(400).send({error: 'Invalid Password'});
+
+    user.password = undefined;
+
+    const token = jwt.sign({ id: user.id }, authConfig.secret, {
+        expiresIn: 86400 //segundos
+    });
+
+    res.send({ user, token});
 });
 
 //repassando a rota com prefixo auth para o app
